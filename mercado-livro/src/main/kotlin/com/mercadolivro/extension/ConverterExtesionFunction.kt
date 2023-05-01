b3c85c71cd2bd4cc3f64f1646dceae87cd9f0220
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-01 11:45:32 -0300

    Feat: Feito created para Book.

--- mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtesionFunction.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/extension/ConverterExtesionFunction.kt
@@ -1,7 +1,10 @@
 package com.mercadolivro.extension
 
+import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
@@ -10,4 +13,13 @@ fun PostCustomerRequest.toCustomerModel(): CustomerModel {
 
 fun PutCustomerRequest.toCustomerModel(id: Int): CustomerModel {
     return CustomerModel(id = id, name = this.name, email = this.email)
+}
+
+fun PostBookRequest.toBookModel(customer: CustomerModel):BookModel {
+    return BookModel(
+        name = this.name,
+        price = this.price,
+        status = BookStatus.ATIVO,
+        cutomer = customer
+    )
 }
