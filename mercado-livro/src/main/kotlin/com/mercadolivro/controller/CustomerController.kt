13a4d89a03af6b37882d85477ef75102333dfb32
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-02 15:46:21 -0300

    Feat: Adicionando campo status para customer.

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -33,7 +33,8 @@ class CustomerController(
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun update(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest) {
-       customerService.update(customer.toCustomerModel(id))
+        val customerSaved = customerService.findById(id)
+       customerService.update(customer.toCustomerModel(customerSaved))
     }
 
     @DeleteMapping("/{id}")
