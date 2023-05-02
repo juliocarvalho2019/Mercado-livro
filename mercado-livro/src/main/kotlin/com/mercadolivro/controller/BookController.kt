e063e9dd4bbe359517e7bea7419b2b63ddea0149
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-02 17:06:55 -0300

    Feat: Adicionado paginação em livros.

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -7,6 +7,9 @@ import com.mercadolivro.extension.toBookModel
 import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -25,13 +28,13 @@ class BookController(
     }
 
     @GetMapping
-    fun findAll(): List<BookResponse> {
-        return bookService.findAll().map { it.toResponse() }
+    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse> {
+        return bookService.findAll(pageable).map { it.toResponse() }
     }
 
     @GetMapping("/active")
-    fun findActives(): List<BookResponse> =
-        bookService.findActives().map { it.toResponse() }
+    fun findActives(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse> =
+        bookService.findActives(pageable).map { it.toResponse() }
 
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Int): BookResponse {
--- mercado-livro/src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -3,11 +3,17 @@ package com.mercadolivro.repository
 import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
+import java.awt.print.Book
 
-interface BookRepository : CrudRepository<BookModel, Int> {
+interface BookRepository : JpaRepository<BookModel, Int> {
 
-    fun findByStatus(status: BookStatus): List<BookModel>
+    fun findByStatus(status: BookStatus, pageable: Pageable): Page<BookModel>
     fun findByCustomer(customer: CustomerModel): List<BookModel>
+
+//    fun findAll(pageable: Pageable): Page<BookModel>
 
 }
