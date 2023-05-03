44e05ea3736c74cb2f230c3743b229639519a8b6
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-03 16:13:40 -0300

    Feat: Validação na requisição com spring validation.

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,9 +1,11 @@
 package com.mercadolivro.controller.request
 
-import com.mercadolivro.model.CustomerModel
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
 
 data class PostCustomerRequest (
+    @field:NotEmpty
     var name: String,
-
+    @field:Email
     var email: String
 )
