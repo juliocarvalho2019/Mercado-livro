b07a3a37398d75d07bae7d99ad5a4fc445284a67
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-04 15:06:08 -0300

    Feat: Implementado swagger para apicação.

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -24,6 +24,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+	//Swagger
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
 	implementation("org.flywaydb:flyway-core:7.7.0")
 
 	runtimeOnly("mysql:mysql-connector-java")
