50139b58a84ebc8f2260f1c72fec74072693dd8d
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-05 10:16:12 -0300

    Feat: Modelado e criado o endpoint purchase para compra.

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -0,0 +1,13 @@
+package com.mercadolivro.controller.request
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+
+data class PostPurchaseRequest(
+    @field:NotNull
+    @field:Positive
+    val customerId: Int,
+
+    @field:NotNull
+    val bookId: Set<Int>
+)
--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PutBookRequest.kt
@@ -1,8 +1,6 @@
 package com.mercadolivro.controller.request
 
 import java.math.BigDecimal
-import javax.validation.constraints.NotEmpty
-import javax.validation.constraints.NotNull
 
 data class PutBookRequest(
     var name: String?,
