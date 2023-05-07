41941787f4a12b6312218ef718cf94270ced59b6
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-07 15:24:21 -0300

    Feat: Iniciando configuração de segurança, liberando algumas rotas como post customer.

--- mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,13 +2,32 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig() : WebSecurityConfigurerAdapter() {
+    private val PUBLIC_MATCHERS = arrayOf<String>()
+    private val PUBLIC_POST_MATCHERS = arrayOf(
+        "/customer"
+    )
+
+    override fun configure(http: HttpSecurity) {
+        http.cors().and().csrf().disable()
+        http.authorizeRequests()
+            .antMatchers(*PUBLIC_POST_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+            .anyRequest().authenticated()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
 
     @Bean
-    fun bCryptPasswordEncoder(): BCryptPasswordEncoder{
+    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
         return BCryptPasswordEncoder()
     }
 }
