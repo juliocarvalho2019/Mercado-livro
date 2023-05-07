6ef3be210d13e38c241732b9f1f66c56cef814bf
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-07 15:05:18 -0300

    Feat: Adiconado criptografia para senha.

--- mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -6,13 +6,14 @@ import com.mercadolivro.enums.Profile
 import com.mercadolivro.exception.NotFoundException
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
-import java.lang.Exception
 
 @Service
 class CustomerService(
-    val customerRepository: CustomerRepository,
-    val bookService: BookService
+    private val customerRepository: CustomerRepository,
+    private  val bookService: BookService,
+    private val bCrypt: BCryptPasswordEncoder
 ) {
 
     fun getAll(name: String?): List<CustomerModel> {
@@ -24,7 +25,8 @@ class CustomerService(
 
     fun create(customer: CustomerModel) {
         val customerCopy = customer.copy(
-            roles = setOf(Profile.CUSTOMER)
+            roles = setOf(Profile.CUSTOMER),
+            password = bCrypt.encode(customer.password)
         )
         customerRepository.save(customerCopy)
     }
