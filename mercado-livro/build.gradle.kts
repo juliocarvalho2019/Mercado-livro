9c56bb858aceb42533bcc8147d07c3dd17ac3659
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-17 09:02:24 -0300

    Feat: Adicionando dependencia mockk.

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {
