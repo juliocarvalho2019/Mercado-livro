fe7f35663857bb3b0ae8fc9e603c1f661008912a
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-19 09:32:54 -0300

    Feat: Criado testes de create e update purchase e utilizando slots.

--- mercado-livro/src/test/kotlin/com/mercadolivro/service/PurchaseServiceTest.kt
+++ mercado-livro/src/test/kotlin/com/mercadolivro/service/PurchaseServiceTest.kt
@@ -1,12 +1,53 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.helper.buildPurchase
+import com.mercadolivro.repository.PurchaseRepository
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.context.ApplicationEventPublisher
 
 @ExtendWith(MockKExtension::class)
-class PurchaseServiceTest{
+class PurchaseServiceTest {
     @MockK
+    private lateinit var purchaseRepository: PurchaseRepository
+
+    @MockK
+    private lateinit var applicationEventPublisher: ApplicationEventPublisher
+
+    @MockK
+    private lateinit var purchaseService: PurchaseService
+
+    val purchaseEventSlot = slot<PurchaseEvent>()
+
+    @Test
+    fun `should create purchase and publish event`() {
+        val purchase = buildPurchase()
+
+        every { purchaseRepository.save(purchase) } returns purchase
+        every { applicationEventPublisher.publishEvent(any()) } just runs
+
+        purchaseService.create(purchase)
+
+        verify(exactly = 1) { purchaseRepository.save(purchase) }
+        verify(exactly = 1) { applicationEventPublisher.publishEvent(capture(purchaseEventSlot)) }
+
+        assertEquals(purchase, purchaseEventSlot.captured.purchaseModel)
+    }
+
+    @Test
+    fun `should update purchase`() {
+        val purchase = buildPurchase()
+
+        every { purchaseRepository.save(purchase) } returns purchase
+
+        purchaseService.update(purchase)
+
+        verify(exactly = 1) { purchaseRepository.save(purchase) }
+    }
 
 }
