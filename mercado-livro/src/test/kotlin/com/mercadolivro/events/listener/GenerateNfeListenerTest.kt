7dbaac0d6c728f2dc0fdfd039875f5c4b409103e
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-20 16:56:25 -0300

    Feat: Criado teste GenerateListener utilizando MockK static.

---
+++ mercado-livro/src/test/kotlin/com/mercadolivro/events/listener/GenerateNfeListenerTest.kt
@@ -0,0 +1,34 @@
+package com.mercadolivro.events.listener
+
+import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.helper.buildPurchase
+import com.mercadolivro.service.PurchaseService
+import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class GenerateNfeListenerTest {
+
+    @MockK
+    private lateinit var purchaseService: PurchaseService
+
+    @InjectMockKs
+    private lateinit var generateNfeListener: GenerateNfeListener
+
+    @Test
+    fun `should generate nfe`() {
+        val purchase = buildPurchase(nfe = null)
+        val fakeNfe = UUID.randomUUID()
+        val purchaseExpected = purchase.copy(nfe = fakeNfe.toString())
+        mockkStatic(UUID::class)
+
+        every { UUID.randomUUID() } returns fakeNfe
+        every { purchaseService.update(purchaseExpected) } just runs
+
+        generateNfeListener.listen(PurchaseEvent(this, purchase))
+
+        verify(exactly = 1) { purchaseService.update(purchaseExpected) }
+    }
+}
