99862bb7545d5aa34785d60ed01627ac3bb2fa9d
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-05-21 09:28:28 -0300

    Feat: Feito teste customerCreate.

--- mercado-livro/src/test/kotlin/com/mercadolivro/controller/CustomerControllerTest.kt
+++ mercado-livro/src/test/kotlin/com/mercadolivro/controller/CustomerControllerTest.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.helper.buildCustomer
 import com.mercadolivro.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
@@ -10,12 +11,14 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import kotlin.random.Random
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -71,5 +74,22 @@ class CustomerControllerTest {
             .andExpect(jsonPath("$[0].status").value(customer1.status.name))
 
     }
+
+    @Test
+    fun `should create customer`() {
+        val request = PostCustomerRequest("fake name", "${Random.nextInt()}@fakeemail.com", "123456")
+
+        mockMvc.perform(post("/customers")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request())))
+            .andExpect(status().isCreated)
+
+        val customers = customerRepository.findAll().toList()
+        assertEquals(1, customers.size)
+        assertEquals(request.name, customers[0].name)
+        assertEquals(request.email, customers[0].email)
+
+    }
+
 
 }
